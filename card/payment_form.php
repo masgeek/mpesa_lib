071471d2422d629e09a623899f8ef90ff0ab4baf
Sammy Barasa <barsamms@gmail.com>
2018-02-19 14:46:34 +0300

    Added cyber source card payment view

--- card/payment_form.php
+++ card/payment_form.php
@@ -6,7 +6,7 @@ $profileID = ' 066620B5-2B5D-49FD-88DC-C4CBA247122A';
 <head>
     <title>Secure Acceptance - Payment Form Example</title>
     <link rel="stylesheet" type="text/css" href="payment.css"/>
-    <script type="text/javascript" src="jquery-1.7.min.js"></script>
+    <script type="text/javascript" src="../vendor/bower-asset/jquery/dist/jquery.min.js"></script>
 </head>
 <body>
 <form id="payment_form" action="payment_confirmation.php" method="post">
@@ -22,7 +22,8 @@ $profileID = ' 066620B5-2B5D-49FD-88DC-C4CBA247122A';
         <legend>Payment Details</legend>
         <div id="paymentDetailsSection" class="section">
             <span>transaction_type:</span><input type="text" name="transaction_type" size="25" value="sale"><br/>
-            <span>reference_number:</span><input type="text" name="reference_number" size="25" value="<?php echo gmdate("YmdHis"); ?>"><br/>
+            <span>reference_number:</span><input type="text" name="reference_number" size="25"
+                                                 value="<?php echo gmdate("YmdHis"); ?>"><br/>
             <span>amount:</span><input type="text" name="amount" value="10.00" size="25"><br/>
             <span>currency:</span><input type="text" name="currency" value="KES" size="25"><br/>
         </div>
