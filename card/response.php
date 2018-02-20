cbe537b8856c2087fe604b35b2f451b9db3faffc
Masgeek <barsamms@gmail.com>
2018-02-20 12:05:23 +0300

    Updated on card and mpesa payment interfaces

---
+++ card/response.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: masgeek
+ * Date: 20-Feb-18
+ * Time: 12:04
+ */
+
+$resp = serialize($_REQUEST);
+
+file_put_contents(date('Y_m_d_his-') . 'response.log', $resp);
