b8ef16cd91c9b8e7535b91f8b1d410c5288e8fb6
Sammy Barasa <barsamms@gmail.com>
2018-02-20 18:40:06 +0300

    moving of files

--- mpesa/index.php
+++ mpesa/index.php
@@ -35,7 +35,7 @@ $timestamp = $mpesa->GetTimeStamp(true);
 $password = base64_encode($BusinessShortCode . $LipaNaMpesaPasskey . $timestamp);
 
 $callbackURL = 'https://smis2.uonbi.ac.ke/mpesa/';
-$callbackParams = "callback.php";
+$callbackParams = "payment/callback.php";
 $lipa_na_mpesa_post = array(
     //Fill in the request parameters with valid values
     'BusinessShortCode' => $BusinessShortCode,
@@ -79,8 +79,8 @@ $lipa_na_mpesa_query_post = array(
 //xdebug_var_dump($lipa_na_mpesa_post);
 
 //$resp = $mpesa->GenerateToken();
-//$resp = $mpesa->LipaNaMpesaProcessRequest($lipa_na_mpesa_post);
-$resp = $mpesa->LipaNaMpesaRequest($lipa_na_mpesa_query_post);
+$resp = $mpesa->LipaNaMpesaProcessRequest($lipa_na_mpesa_post);
+//$resp = $mpesa->LipaNaMpesaRequest($lipa_na_mpesa_query_post);
 //$resp = $mpesa->ConsumerToBusinessSimulate($c2b_post_data);
 ///$decoded = \mpesa\TRANSACTION_CALLBACKS::processSTKPushQueryRequestCallback($resp);
 //var_dump($decoded);
