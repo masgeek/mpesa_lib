59aa618afd37da6a76f8bd5b6b6e77f35019ad4a
Sammy Barasa <barsamms@gmail.com>
2018-02-20 18:42:12 +0300

    moving of files

--- mpesa/callback.php
+++ mpesa/callback.php
@@ -6,5 +6,11 @@
  * Time: 11:56
  */
 
-$req_dump = print_r( $_REQUEST, true );
-$fp = file_put_contents( date('Y_m_d').'request.log',serialize($req_dump) );
+$req_dumpA = print_r( $_GET, true );
+$req_dumpB = print_r( $_POST, true );
+$req_dumpC = print_r( $_REQUEST, true );
+
+
+file_put_contents( date('Y_m_d').'request_get.log',serialize($req_dumpA) );
+file_put_contents( date('Y_m_d').'request_post.log',serialize($req_dumpB) );
+file_put_contents( date('Y_m_d').'request_request.log',serialize($req_dumpC) );
