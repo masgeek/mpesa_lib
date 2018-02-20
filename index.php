ad306bdc7f803d78fb1841d1260f84af9f2339d9
Masgeek <barsamms@gmail.com>
2018-02-20 14:59:05 +0300

    Updated on card and mpesa payment interfaces

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
                         <i class="fa fa-mobile-phone fa-4x"></i>
                     </p>
                 </div>
-                <div class="card-footer"><a href="card/index.php" class="btn btn-outline-success btn-block btn-lg">MOBILE MONEY</a>
+                <div class="card-footer"><a href="mpesa/" class="btn btn-outline-success btn-block btn-lg">MOBILE MONEY</a>
                 </div>
             </div>
         </div>
