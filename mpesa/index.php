29c9572d25d6ca6a2c68695ee9d8918782c3dccd
Sammy Barasa <barsamms@gmail.com>
2018-02-20 18:36:29 +0300

    moving of files

--- mpesa/index.php
+++ mpesa/index.php
@@ -79,8 +79,8 @@ $lipa_na_mpesa_query_post = array(
 //xdebug_var_dump($lipa_na_mpesa_post);
 
 //$resp = $mpesa->GenerateToken();
-//$resp = $mpesa->LipaNaMpesaProcessRequest($lipa_na_mpesa_post);
-$resp = $mpesa->LipaNaMpesaRequest($lipa_na_mpesa_query_post);
+$resp = $mpesa->LipaNaMpesaProcessRequest($lipa_na_mpesa_post);
+//$resp = $mpesa->LipaNaMpesaRequest($lipa_na_mpesa_query_post);
 //$resp = $mpesa->ConsumerToBusinessSimulate($c2b_post_data);
 ///$decoded = \mpesa\TRANSACTION_CALLBACKS::processSTKPushQueryRequestCallback($resp);
 //var_dump($decoded);
