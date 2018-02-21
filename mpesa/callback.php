db7a128db8ae454767d8cc00786b3e1c9a884aa9
Masgeek <barsamms@gmail.com>
2018-02-21 14:09:40 +0300

    Updated phone number mask

--- mpesa/callback.php
+++ mpesa/callback.php
@@ -5,8 +5,24 @@
  * Date: 08-Feb-18
  * Time: 11:56
  */
+$root_dir = dirname(dirname(__FILE__));
 
-$callbackJSONData=file_get_contents('php://input');
+use Dotenv\Dotenv;
+use GuzzleHttp\Client;
+use Httpful\Request;
+
+require_once $root_dir . '/vendor/autoload.php';
+
+$callbackJSONData = file_get_contents('php://input');
 
 
+var_dump($callbackJSONData);
 file_put_contents('logs/' . date('Y_m_d_his-') . 'request_json.log', $callbackJSONData);
+
+class callback
+{
+    public static function WriteSTKToDatabase($callbackObject)
+    {
+
+    }
+}
