88962df33cf444e685994f6fcb61cbda37f6e0a7
Masgeek <barsamms@gmail.com>
2018-02-21 11:50:25 +0300

    Added error handler page

--- card/callback.php
+++ card/callback.php
@@ -1,5 +1,5 @@
 <?php
 $resp = serialize($_REQUEST);
-$fp = file_put_contents(date('Y_m_d_his-') . 'callback.log', $resp);
+$fp = file_put_contents('logs/' . date('Y_m_d_his-') . 'callback.log', $resp);
 echo '<pre>';
 var_dump(unserialize($resp));
