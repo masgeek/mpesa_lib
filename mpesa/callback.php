786bf936bb8f14701851468249a17c2cbed38941
Masgeek <barsamms@gmail.com>
2018-02-21 14:07:26 +0300

    Updated phone number mask

--- mpesa/callback.php
+++ mpesa/callback.php
@@ -6,13 +6,7 @@
  * Time: 11:56
  */
 
-$req_dumpA = print_r($_GET, true);
-$req_dumpB = print_r($_POST, true);
-$req_dumpC = print_r($_REQUEST, true);
 $callbackJSONData=file_get_contents('php://input');
 
 
-file_put_contents('logs/' . date('Y_m_d') . 'request_get.log', serialize($req_dumpA));
-file_put_contents('logs/' . date('Y_m_d') . 'request_post.log', serialize($req_dumpB));
-file_put_contents('logs/' . date('Y_m_d') . 'request_request.log', serialize($req_dumpC));
-file_put_contents('logs/' . date('Y_m_d') . 'request_json.log', $callbackJSONData);
+file_put_contents('logs/' . date('Y_m_d_his-') . 'request_json.log', $callbackJSONData);
