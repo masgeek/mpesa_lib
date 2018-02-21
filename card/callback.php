82af51cec6c68f4be3271696a8318f2a6c4e7e1f
Masgeek <barsamms@gmail.com>
2018-02-21 15:59:30 +0300

    Renamed incorrectly named file

--- card/callback.php
+++ card/callback.php
@@ -1,10 +1,4 @@
 <?php
-$respA = serialize($_REQUEST);
-$respB = serialize($_POST);
-$respC = serialize($_GET);
-$respD = file_get_contents('php://input');
+$callbackParams = serialize($_POST);
 
-$fp = file_put_contents('logs/' . date('Y_m_d_his-') . 'callbackREQUEST.log', $respA);
-$fp = file_put_contents('logs/' . date('Y_m_d_his-') . 'callbackPOST.log', $respB);
-$fp = file_put_contents('logs/' . date('Y_m_d_his-') . 'callbackGET.log', $respC);
-$fp = file_put_contents('logs/' . date('Y_m_d_his-') . 'callbackFILE.log', $respD);
+$fp = file_put_contents('logs/' . date('Y_m_d_his-') . 'callbackPOST.log', $callbackParams);
